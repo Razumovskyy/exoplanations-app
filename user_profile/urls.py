ab9a995f6c14f67e3394966d1211d619846e8a7d
Razumovskyy <mrazumovskyy@gmail.com>
2023-01-19 23:07:34 +0600

    first simple API

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from .views import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'profile', ProfileViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls'))
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .serializers import ProfileSerializer
+from .models import UserProfile
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+class ProfileViewSet(viewsets.ModelViewSet):
+
+    queryset = UserProfile.objects.all()
+    serializer_class = ProfileSerializer
