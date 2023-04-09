ee119402182d90c2df6f301f854d9155b7931237
Razumovskyy <mrazumovskyy@gmail.com>
2023-04-09 20:46:52 +0600

    removed asgi.py

--- user_profile/models.py
+++ user_profile/models.py
@@ -6,5 +6,3 @@ class UserProfile(models.Model):
     name = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField()
     
-    def __str__(self):
-        return self.title
